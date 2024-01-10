7ee1e6f62c0c013a0263e26c6f1e259cd13e2655
Nathan <lalaqdland@gmail.com>
2024-01-10 23:25:54 +0800

    second commit

--- com.itheima.JAVA/src/com/itheima/JAVA/GitTest.java
+++ com.itheima.JAVA/src/com/itheima/JAVA/GitTest.java
@@ -12,5 +12,6 @@ package com.itheima.JAVA;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
+        System.out.println("Hello, Git2!");
     }
 }
